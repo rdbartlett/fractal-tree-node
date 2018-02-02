374bcdcb9a8654af440101b345d5ee95d15f0dc5
Richard Bartlett <rich@loomio.org>
2018-02-02 20:17:58 +1300

    add play and pause

--- js/animate.js
+++ js/animate.js
@@ -1,74 +1,28 @@
 var stateMgmt = require('./stateMgmt')
 var drawCanvas = require('./drawCanvas')
+// If widthh is sweeping from 20 to 50 degrees and back to 20
+// in a period of 10 frames,
+// the delta is 6 degrees-per-frame [(50-20)/(10/2)].
+// If quirkk is sweeping from 10-20 over 5 frames, the delta is 4.
+//
+//         0     1     2     3     4     5     6     7     8     9     10
+// Frames: | - - | - - | - - | - - | - - | - - | - - | - - | - - | - - |
+// Widthh: 20    26    32    38    44    50    44    38    32    26    20
+// Quirkk: 10    14    18    18    14    10    14    18    18    14    10
+//
 
-// var recurringTimer
-// var oneShotTimer
-
-// playing = true
-
-// scene1 = {from: 100, to: 50,  rate: 50}
-// scene2 = {from:  50, to: 100, rate: 20}
-// scene3 = {from: 100, to: 100, rate: 500}
-// sequence = [ scene1, scene2, scene3 ]
-
-// function playOrPause(){
-//   if(playing) {
-//     playing = !playing
-//     startSequence()
-//   }
-//   else {
-//     playing = !playing
-//     clearTimeout(oneShotTimer)
-//     clearTimeout(recurringTimer)
-//   }
-// }
-
-// function startSequence() {
-//   thisScene = 0;
-//   animate(sequence[thisScene])
-// }
-
-// function advanceSequence(){
-//   thisScene++
-//   if(sequence.length == thisScene) thisScene = 0
-//   animate(sequence[thisScene])
-// }
-
-// function animate(scene){
-//   count = 1
-//   next = scene.from
-//   hold = false
-
-//   if (scene.from > scene.to) growOrShrink = 1
-//   else if (scene.from < scene.to) growOrShrink = -1
-//   else hold = true;
-
-//   if(hold) oneShotTimer = setTimeout(advanceSequence, scene.rate)
-//   else recurringTimer = setInterval(tween, scene.rate);
-
-//   function tween(){
-//     if (next == scene.to){
-//       clearInterval(recurringTimer)
-//       count = 1
-//       advanceSequence();
-//     } else {
-//       next = scene.from - (count*growOrShrink)
-//       stateMgmt.set('widthh', next);
-//       drawCanvas.fromState();
-
-//       if(state.orbitt) stateMgmt.inc('angle', 1);
-//       count++
-//     }
-//   }
-// }
-
-// ----------------
-
-var frameRate = 33
+const frameRate = 33
 var ranges = {
   quirkk: {low: -40,  high: 40,   period: 120},
   widthh: {low:  30,  high: 60,   period: 80}
 }
+const attrs = ['quirkk', 'widthh']
+var deltas = []
+attrs.forEach(function(attr){
+  deltas[attr] = (ranges[attr].high - ranges[attr].low)/ (ranges[attr].period / 2)
+});
+
+
 var sweepTimer
 var growOrShrink = {
   quirkk: true,
@@ -79,41 +33,38 @@ var growOrShrink = {
   yessss: true
 }
 
-var deltas = []
-
 function sweep(){
-  stateMgmt.set('quirkk', ranges['quirkk'].low)
-  drawCanvas.fromState();
-
-  deltas['quirkk'] = (ranges['quirkk'].high - ranges['quirkk'].low)/ (ranges['quirkk'].period / 2)
-  deltas['widthh'] = (ranges['widthh'].high - ranges['widthh'].low)/ (ranges['widthh'].period / 2)
-
   sweepTimer = setInterval(function(){ nextFrame() }, frameRate)
 }
 
 function nextFrame(){
-  var attr = 'quirkk'
-  if(growOrShrink[attr]){
-    if(state[attr] < ranges[attr].high) stateMgmt.inc(attr, deltas[attr])
-    else growOrShrink[attr] = !growOrShrink[attr]
-  }
-  else{
-    if(state[attr] > ranges[attr].low) stateMgmt.dec(attr, deltas[attr])
-    else growOrShrink[attr] = !growOrShrink[attr]
-  }
+  attrs.forEach(function(attr){
+    if(growOrShrink[attr]){
+      if(state[attr] < ranges[attr].high) stateMgmt.inc(attr, deltas[attr])
+      else growOrShrink[attr] = !growOrShrink[attr]
+    }
+    else{
+      if(state[attr] > ranges[attr].low) stateMgmt.dec(attr, deltas[attr])
+      else growOrShrink[attr] = !growOrShrink[attr]
+    }
+  });
 
-  var attr = 'widthh'
-  if(growOrShrink[attr]){
-    if(state[attr] < ranges[attr].high) stateMgmt.inc(attr, deltas[attr])
-    else growOrShrink[attr] = !growOrShrink[attr]
-  }
-  else{
-    if(state[attr] > ranges[attr].low) stateMgmt.dec(attr, deltas[attr])
-    else growOrShrink[attr] = !growOrShrink[attr]
-  }
+  if(state.orbitt) stateMgmt.inc('angle', 1);
 
   drawCanvas.fromState();
 }
 
-// module.exports = {startSequence, playOrPause, sweep}
-module.exports = {sweep}
+playing = false
+
+function playOrPause(){
+  if(playing) {
+    playing = !playing
+    sweep()
+  }
+  else {
+    playing = !playing
+    clearTimeout(sweepTimer)
+  }
+}
+
+module.exports = {sweep, playOrPause}
