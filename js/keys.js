ac0ea1f09dbaf643f4febace3a3afb421c2076ca
Richard Bartlett <rich@loomio.org>
2018-02-02 15:41:34 +1300

    set keys

--- js/keys.js
+++ js/keys.js
@@ -1,5 +1,24 @@
 var Mousetrap = require('mousetrap')
 var stateMgmt = require('./stateMgmt')
+var drawCanvas = require('./drawCanvas')
 
-// Mousetrap.bind('q', function() { stateMgmt.inc('quirkk', 1) });
-Mousetrap.bind('q', function() { alert('yo') });
+Mousetrap.bind('q', function() { stateMgmt.inc('quirkk', 1); drawCanvas.fromState() });
+Mousetrap.bind('w', function() { stateMgmt.inc('widthh', 1); drawCanvas.fromState() });
+Mousetrap.bind('e', function() { stateMgmt.inc('energy', 0.05); drawCanvas.fromState() });
+Mousetrap.bind('r', function() { stateMgmt.inc('repeat', 1); drawCanvas.fromState() });
+Mousetrap.bind('t', function() { stateMgmt.inc('tensor', 1); drawCanvas.fromState() });
+Mousetrap.bind('y', function() { stateMgmt.inc('yessss', 1); drawCanvas.fromState() });
+Mousetrap.bind('a', function() { stateMgmt.dec('quirkk', 1); drawCanvas.fromState() });
+Mousetrap.bind('s', function() { stateMgmt.dec('widthh', 1); drawCanvas.fromState() });
+Mousetrap.bind('d', function() { stateMgmt.dec('energy', 0.05); drawCanvas.fromState() });
+Mousetrap.bind('f', function() { stateMgmt.dec('repeat', 1); drawCanvas.fromState() });
+Mousetrap.bind('g', function() { stateMgmt.dec('tensor', 1); drawCanvas.fromState() });
+Mousetrap.bind('h', function() { stateMgmt.dec('yessss', 1); drawCanvas.fromState() });
+Mousetrap.bind('o', function() { toggle('orbitt') });
+Mousetrap.bind('p', function() { toggle('points') });
+
+function toggle(attr){
+  if(state[attr]) stateMgmt.set(attr, false)
+  else stateMgmt.set(attr, true)
+  drawCanvas.fromState()
+}
--- js/stateMgmt.js
+++ js/stateMgmt.js
@@ -6,6 +6,7 @@ function init(){
   color: '#D711B4',
   angle: -90, pointSize: 2, lineWidth: 1, points: true, orbitt: true
   }
+  updateReader()
   return state
 }
 
@@ -31,7 +32,7 @@ function dec(attr, by){
 function updateReader(){
   document.getElementById('quirkk').textContent = state.quirkk;
   document.getElementById('widthh').textContent = state.widthh;
-  document.getElementById('energy').textContent = state.energy;
+  document.getElementById('energy').textContent = state.energy.toFixed(2);
   document.getElementById('repeat').textContent = state.repeat;
   document.getElementById('tensor').textContent = state.tensor;
   document.getElementById('yessss').textContent = state.yessss;
@@ -39,4 +40,4 @@ function updateReader(){
   document.getElementById('points').textContent = state.points.toString();
 }
 
-module.exports = { init, get, set, inc, dec }
+module.exports = { init, get, set, inc, dec}
