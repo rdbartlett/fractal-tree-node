06e34c4eb8d8ebdf4904ef6a878f629d9ca93286
Richard Bartlett <rich@loomio.org>
2018-05-25 20:30:39 +0200

    remove reset after attr change

--- js/presets.js
+++ js/presets.js
@@ -7,7 +7,7 @@ var presets = [
   , {repeat: 6,  yessss: 14, widthh: 60,  tensor: 20, quirkk: 0,   energy: 15}
   , {repeat: 5,  yessss: 9,  widthh: 180, tensor: 20, quirkk: -60, energy: 25}
   , {repeat: 6,  yessss: 9,  widthh: 60,  tensor: 8,  quirkk: 0,   energy: 60}
-  , {repeat: 13, yessss: 7,  widthh: 90,  tensor: 11, quirkk: 0,   energy: 50}
+  , {repeat: 16, yessss: 13, widthh: 60,  tensor: 13, quirkk: 0,   energy: 20}
   , {repeat: 3,  yessss: 7,  widthh: 6,   tensor: 4,  quirkk: -90, energy: 80}
   , {repeat: 4,  yessss: 10, widthh: 45,  tensor: 8,  quirkk: 90,  energy: 40}
   , {repeat: 7,  yessss: 11, widthh: 90,  tensor: 13, quirkk: 0,   energy: 50}
@@ -16,12 +16,12 @@ var presets = [
 
 function load(index){
   var newState = presets[index]
-  rangesMgmt.set('quirkk', 'low', newState.quirkk)
-  rangesMgmt.set('widthh', 'low', newState.widthh)
-  rangesMgmt.set('energy', 'low', newState.energy)
-  rangesMgmt.set('repeat', 'low', newState.repeat)
-  rangesMgmt.set('tensor', 'low', newState.tensor)
-  rangesMgmt.set('yessss', 'low', newState.yessss)
+  rangesMgmt.set('quirkk', 'center', newState.quirkk)
+  rangesMgmt.set('widthh', 'center', newState.widthh)
+  rangesMgmt.set('energy', 'center', newState.energy)
+  rangesMgmt.set('repeat', 'center', newState.repeat)
+  rangesMgmt.set('tensor', 'center', newState.tensor)
+  rangesMgmt.set('yessss', 'center', newState.yessss)
   stateMgmt.set('quirkk', newState.quirkk)
   stateMgmt.set('widthh', newState.widthh)
   stateMgmt.set('energy', newState.energy)
